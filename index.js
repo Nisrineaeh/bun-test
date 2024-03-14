344364cc1d8e57fc90f5b799a241fb43007f3aee
Nisrine <nisrineaitelhaj@yahoo.fr>
2024-03-14 16:51:37 +0100

    test lodash

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+import _ from 'lodash';
+
+let users = [
+    { 'user': 'fred', 'age': 48 },
+    { 'user': 'barney', 'age': 36 },
+    { 'user': 'fred', 'age': 40 },
+    { 'user': 'barney', 'age': 34 }
+];
+
+let sortedUsers = _.sortBy(users, [function (o) { return o.age; }]);
+console.log(sortedUsers);
